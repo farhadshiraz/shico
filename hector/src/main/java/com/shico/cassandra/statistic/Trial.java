50fc8c87f62ada7ca4cb56a76571b1d2e94695c0
Farhad Dehghani <farhad@shirazconsult.com>
2012-05-17 20:31:00 +0200

    With TimeUUIDType

--- hector/src/main/java/com/shico/cassandra/statistic/Trial.java
+++ hector/src/main/java/com/shico/cassandra/statistic/Trial.java
@@ -1,6 +1,9 @@
 package com.shico.cassandra.statistic;
 
 import java.nio.ByteBuffer;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,6 +12,7 @@ import java.util.UUID;
 
 import me.prettyprint.cassandra.model.HColumnImpl;
 import me.prettyprint.cassandra.model.IndexedSlicesQuery;
+import me.prettyprint.cassandra.serializers.ByteBufferSerializer;
 import me.prettyprint.cassandra.serializers.LongSerializer;
 import me.prettyprint.cassandra.serializers.StringSerializer;
 import me.prettyprint.cassandra.serializers.UUIDSerializer;
@@ -55,21 +59,33 @@ public class Trial {
         try {
         	// Insert event records
             long now = System.currentTimeMillis();
-            for (int i=1; i<=(NUMBER_OF_ROWS/BATCH_SIZE); i++) {
-            	Mutator<UUID> mutator = HFactory.createMutator(keyspace, UUIDSerializer.get());
-            	for (int j = 1; j <= BATCH_SIZE; j++) {
-            		UUID rowKey = UUID.randomUUID();
-            		addRandomEvents(mutator, rowKey, i*j, now-(i+j*5));
-				}
-            	mutator.execute();
-			}
-            
+//            for (int i=1; i<=(NUMBER_OF_ROWS/BATCH_SIZE); i++) {
+//            	Mutator<UUID> mutator = HFactory.createMutator(keyspace, UUIDSerializer.get());
+//            	for (int j = 1; j <= BATCH_SIZE; j++) {
+//            		UUID rowKey = UUID.randomUUID();
+//            		addRandomEvents(mutator, rowKey, i*j, now-(i+j*5));
+//				}
+//            	mutator.execute();
+//			}
+
+            Mutator<UUID> mutator = HFactory.createMutator(keyspace, UUIDSerializer.get());
+            UUID key = UUID.randomUUID();
+            addStringColumn(mutator, key, "event_type", CHANNEL_CHANGE_STB);
+    		addStringColumn(mutator, key, "customer_ref", "cusotmer_"+1);
+    		addStringColumn(mutator, key, "channel_ref", "channel_"+1);
+    		addStringColumn(mutator, key, "device_ref", "device_"+1);
+    		addLongColumn(mutator, key, "duration", 1+100L);
+    		addDateColumn(mutator, key, "event_time", System.currentTimeMillis());
+    		mutator.execute();
             findByEventType(CHANNEL_CHANGE_STB, keyspace, "event_type", "customer_ref", "channel_ref", "device_ref", "duration", "event_time");
             
-        } catch (HectorException e) {
-            e.printStackTrace();
+        } catch (HectorException he) {
+            he.printStackTrace();
+        }catch(Exception e){
+        	e.printStackTrace();
+        }finally{
+        	getTestCluster().getConnectionManager().shutdown();
         }
-        getTestCluster().getConnectionManager().shutdown();
     }
 
 	private static Cluster getTestCluster(){
@@ -77,32 +93,46 @@ public class Trial {
 	}
 	
 	private static void findByEventType(String eventType, Keyspace keyspace, String...columnNames){
-        IndexedSlicesQuery<UUID, String, String> query = HFactory.createIndexedSlicesQuery(keyspace, uuidse, sse, sse);
+        IndexedSlicesQuery<UUID,String,ByteBuffer> query = HFactory.createIndexedSlicesQuery(keyspace, uuidse, StringSerializer.get(), ByteBufferSerializer.get());
         query.setColumnFamily(COLUMN_FAMILY);
-        List<String> queryColumns = new ArrayList<String>();
-        for (String colName : columnNames) {
-			if(getColumnType(colName).equals("org.apache.cassandra.db.marshal.UTF8Type")){
-				queryColumns.add(colName);
-			}
-		}
-        query.setColumnNames(queryColumns);
-        query.addEqualsExpression("event_type", eventType);
-        QueryResult<OrderedRows<UUID, String, String>> result = query.execute();
+//        List<String> queryColumns = new ArrayList<String>();
+//        for (String colName : columnNames) {
+//			if(getColumnType(colName).equals("org.apache.cassandra.db.marshal.UTF8Type")){
+//				queryColumns.add(colName);
+//			}
+//		}
+        query.setColumnNames(columnNames);
+        query.addEqualsExpression("event_type", ByteBuffer.wrap(eventType.getBytes()));
+        QueryResult<OrderedRows<UUID, String, ByteBuffer>> result = query.execute();
                     
-        OrderedRows<UUID, String, String> rows = result.get();
-        List<Row<UUID, String, String>> list = rows.getList();
+        List<Row<UUID, String, ByteBuffer>> list = result.get().getList();
         StringBuilder sb = new StringBuilder();
-        for (Row<UUID, String, String> row : list) {
+        for (Row<UUID, String, ByteBuffer> row : list) {
         	sb.append("key=").append(row.getKey()).append("{");
-        	ColumnSlice<String, String> columnSlice = row.getColumnSlice();
-        	List<HColumn<String, String>> columns = columnSlice.getColumns();
-        	for (HColumn<String, String> hColumn : columns) {
-        		sb.append("[").append(hColumn.getName()).append("=").append(hColumn.getValue()).append("]");
+        	ColumnSlice<String, ByteBuffer> columnSlice = row.getColumnSlice();
+        	List<HColumn<String, ByteBuffer>> columns = columnSlice.getColumns();
+        	for (HColumn<String, ByteBuffer> hColumn : columns) {
+        		sb.append("[").append(hColumn.getName()).append("=").append(columnValueAsString(hColumn)).append("]");
 			}
         	sb.append("}\n");
 		}
         System.out.println("Result: ");
         System.out.println(sb.toString());		
+	}
+	
+	private static DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+	private static String columnValueAsString(HColumn<String, ByteBuffer> column){
+		String type = getColumnType(column.getName());
+		if(type.equals("org.apache.cassandra.db.marshal.UTF8Type")){
+			return byteBufferAsString(column.getValue());
+		}else if(type.equals("org.apache.cassandra.db.marshal.LongType")){
+			return String.valueOf(column.getValue().asLongBuffer().get());
+		}else if(type.equals("org.apache.cassandra.db.marshal.TimeUUIDType")){
+			UUID uuid = TimeUUIDUtils.uuid(column.getValueBytes());
+			long timeFromUUID = TimeUUIDUtils.getTimeFromUUID(uuid);
+			return df.format(new Date(timeFromUUID));
+		}
+		return byteBufferAsString(column.getValue());
 	}
 	
 	private static String getColumnType(String name){
@@ -111,6 +141,7 @@ public class Trial {
 	
 	private static Map<String, String> getColumnDefinitions(){
 		if(columnDefinitions == null){
+			columnDefinitions = new HashMap<String, String>();
 			KeyspaceDefinition ksdef = getTestCluster().describeKeyspace(KEYSPACE);
 			List<ColumnFamilyDefinition> cfDefs = ksdef.getCfDefs();
 			for (ColumnFamilyDefinition cfdef : cfDefs) {
@@ -174,6 +205,7 @@ public class Trial {
         HColumn<String, String> col = new HColumnImpl<String, String>(sse, sse);
         col.setName(name);
         col.setValue(value);
+        col.setClock(System.currentTimeMillis());
         mutator.addInsertion(key, COLUMN_FAMILY, col);		
 	}
 
@@ -181,6 +213,7 @@ public class Trial {
 		HColumn<String, Long> col = new HColumnImpl<String, Long>(sse, LongSerializer.get());
 		col.setName(name);
 		col.setValue(value);
+		col.setClock(System.currentTimeMillis());
 		mutator.addInsertion(key, COLUMN_FAMILY, col);
 	}
 
@@ -188,6 +221,7 @@ public class Trial {
 		HColumn<String, UUID> col = new HColumnImpl<String, UUID>(sse, UUIDSerializer.get());
 		col.setName(name);
 		col.setValue(TimeUUIDUtils.getTimeUUID(value));
+		col.setClock(System.currentTimeMillis());
 		mutator.addInsertion(key, COLUMN_FAMILY, col);
 	}
 
